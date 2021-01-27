26c83c45aff1e40a46211bc4088e688ef2209882
dustin <chevalier.wyz@gmail.com>
2021-01-26 22:54:44 -0500

    startup files

---
+++ a1/graphics.h
@@ -0,0 +1,24 @@
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#endif
+
+#ifdef __LINUX__
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+        /* world size and storage array */
+#define WORLDX 100
+#define WORLDY 50
+#define WORLDZ 100
+
+	/* list of cubes to draw with each screen update */
+#define MAX_DISPLAY_LIST 500000
+
+	/* maximum number of user defined colours */
+#define NUMBERCOLOURS 100
+
